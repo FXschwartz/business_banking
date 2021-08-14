26c3877212a0d5c49f56ee7e27c5ba9b3ac1b06e
Preston <preston.schwartz505@gmail.com>
2021-08-14 11:15:44 -0500

    Cleaned up support request UI, added support request button.

--- business_banking/lib/features/support_request/ui/support_request/support_request_screen.dart
+++ business_banking/lib/features/support_request/ui/support_request/support_request_screen.dart
@@ -13,13 +13,68 @@ class SupportRequestScreen extends Screen {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height * .30,
       padding: EdgeInsets.all(5.0),
-      child: InkWell(
-        onTap: () {
-          this.actions.navigateToSupportRequestForm(context);
-        },
-        child: Center(
-          child: Text('Support Request'),
+      child: Card(
+        key: Key('supportRequest'),
+        color: Colors.white,
+        shadowColor: Colors.grey[500],
+        elevation: 3.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Need Help?',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Submit a support request and someone will reach out to you as quickly as possible',
+                      style: TextStyle(color: Colors.black54, fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      key: Key('supportRequestNavButton'),
+                      child: Text(
+                        'Create Support Request',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          side: BorderSide(width: 2, color: Colors.green)),
+                      onPressed: () {
+                        this.actions.navigateToSupportRequestForm(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- business_banking/lib/features/support_request/ui/support_request_form/support_request_form_screen.dart
+++ business_banking/lib/features/support_request/ui/support_request_form/support_request_form_screen.dart
@@ -1,7 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:business_banking/features/support_request/model/support_request_view_model.dart';
 import 'package:business_banking/features/support_request/ui/support_request_form/support_request_form_actions.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SupportRequestFormScreen extends Screen {
   final SupportRequestViewModel viewModel;
@@ -11,10 +13,35 @@ class SupportRequestFormScreen extends Screen {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5.0),
-      child: Center(
-        child: Text('Support Request Form Screen'),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        leading: GestureDetector(
+          child: Icon(
+            Icons.chevron_left,
+            size: 40.0,
+          ),
+          onTap: () {
+            this.actions.navigateBack(context);
+          },
+          key: Key('backButton'),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            AutoSizeText(
+              'Submit Support Request',
+              key: Key('supportRequestFormTitle'),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(5.0),
+        child: Center(
+          child: Text('Support Request Form Screen'),
+        ),
       ),
     );
   }
